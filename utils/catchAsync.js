9b6240258db496671a98c38d1b9b74c9ca20849f
mohamednazm-web <mhamadnazm53@gmail.com>
2021-06-26 23:10:23 +0300

    test1

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,7 +1,7 @@
 module.exports = fn => {
     return (req, res, next) => {
         fn(req, res, next).catch(err => {
-            res.redirect('/500');
+            res.redirect('/404');
         });
     };
 };
